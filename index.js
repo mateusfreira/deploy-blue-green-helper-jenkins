f10e5efbd88fb1644c9c6a783b2d07b1940b7f57
Mateus Freira <mateus.freira@gmail.com>
2016-04-02 12:13:01 -0300

    Bluebird insteadpf Q

--- index.js
+++ index.js
@@ -1,72 +1,50 @@
-const Q = require('q'),
+const Promise = require("bluebird"),
   _ = require('lodash'),
   rp = require('request-promise'),
   jenkinsapi = require('jenkins-api'),
   deployerManager = require("deploy-blue-green-helper");
 
-var JenkinsDeployer = function (jenkinsOption, enviromentOptions) {
+const JenkinsDeployer = function (jenkinsOption, enviromentOptions) {
   const jenkins = jenkinsapi.init(jenkinsOption.jenkinsUrl),
     self = this,
     log = function (message) {
       if (jenkinsOption.debug) {
         console.log(message);
       }
-    };
-
+    },
+    jenkinsPromise = Promise.promisifyAll(jenkins);
   this.getBuildInfo = function () {
     log(["geting BuildInfo ", enviromentOptions]);
-    var defer = Q.defer();
-    jenkins.last_build_info(enviromentOptions.jobName, function (err, data) {
-      log(["BuildInfo reponse", err, data]);
-      if (err) {
-        defer.reject(err);
-      } else {
-        defer.resolve(data);
-      }
+    return jenkinsPromise.last_build_infoAsync(enviromentOptions.jobName).then(function (data) {
+      log(["BuildInfo reponse", data]);
+      return data;
     });
-    return defer.promise;
   };
 
   this.run = function () {
     log("Ruining build " + enviromentOptions.jobName);
-    var defer = Q.defer();
-    jenkins.build(enviromentOptions.jobName, function (err, data) {
-      if (!err) {
-        log(["No errors ruining build " + enviromentOptions.jobName, data]);
-        defer.resolve(data);
-      } else {
-        log(["Some error happen ruining build " + enviromentOptions.jobName, err]);
-        defer.reject(err);
-      }
+    return jenkinsPromise.buildAsync(enviromentOptions.jobName).then(function (data) {
+      log(["No errors ruining build " + enviromentOptions.jobName, data]);
+      return data;
+    }).catch(function (err) {
+      log(["Some error happen ruining build " + enviromentOptions.jobName, err]);
     });
-    return defer.promise;
   };
 
   this.stop = function (userOptions) {
     log(["Stoping build " + enviromentOptions.jobName]);
-    var defer = Q.defer();
-    self.getBuildInfo().then(function (data) {
-      try {
-        jenkins.stop_build(enviromentOptions.jobName, data.number, function (e, r) {
-          if (!e) {
-            log(["No errors stoping build " + enviromentOptions.jobName]);
-            defer.resolve(r);
-          } else {
-            log(["Some error happen stoping build " + enviromentOptions.jobName, e]);
-            defer.reject(e);
-          }
-
-
-        });
-      } catch (e) {
-        defer.reject(e);
-      }
+    return self.getBuildInfo().then(function (data) {
+      return jenkinsPromise.stop_buildAsync(enviromentOptions.jobName, data.number);
+    }).then(function (r) {
+      log(["No errors stoping build " + enviromentOptions.jobName]);
+      return r;
+    }).catch(function (e) {
+      log(["Some error happen stoping build " + enviromentOptions.jobName, e]);
+      return e;
     });
-    return defer.promise;
   };
 
   this.isRuning = function (userOptions) {
-
     return this.getBuildInfo().then(function (data) {
       return data.building;
     });
